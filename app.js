9ddd6aa435f35eaca89dee42be9d459fed6c1a35
sonia <soniaummehabiba@gmail.com>
2019-10-08 17:00:18 +0500

    changes

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 
 const mongoose = require('mongoose');
-const uri = "mongodb://dbuser:dbuser@cluster0-shard-00-00-twxqe.mongodb.net:27017,cluster0-shard-00-01-twxqe.mongodb.net:27017,cluster0-shard-00-02-twxqe.mongodb.net:27017/admin?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority";
+const uri = "mongodb+srv://dbuser:dbuser@cluster0-twxqe.mongodb.net/admin?retryWrites=true&w=majority";
 mongoose.connect(uri, {useNewUrlParser: true});
 const db = mongoose.connection;
 db.on('error', (err) => console.log(`connection error: ${err}`));
