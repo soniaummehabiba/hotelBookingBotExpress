c040d34178336f574fdc0dc1b3254a98bdcdf17c
sonia <soniaummehabiba@gmail.com>
2019-10-08 22:32:36 +0500

    changes

--- routes/index.js
+++ routes/index.js
@@ -12,7 +12,7 @@ router.post('/webhook', function (req, res, next) {
     const agent = new WebhookClient({request: req, response: res});
 
     // console.log('Dialogflow Request headers >> ' + JSON.stringify(req.headers));
-    console.log('Dialogflow Request body >> ' + JSON.stringify(req.body));
+    // console.log('Dialogflow Request body >> ' + JSON.stringify(req.body));
 
     let intentMap = new Map();
 
